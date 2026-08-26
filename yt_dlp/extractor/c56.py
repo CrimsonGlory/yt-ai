3a76cac975b9e5189320a70d25c09a86209f7fd0
CrimsonGlory <javierbassi@gmail.com>
2026-08-26 18:03:05 +0000

    [56.com] Fix live extractor test

--- yt_dlp/extractor/c56.py
+++ yt_dlp/extractor/c56.py
@@ -1,11 +1,26 @@
 from .common import InfoExtractor
-from ..utils import js_to_json
+from ..utils import ExtractorError, js_to_json
 
 
 class C56IE(InfoExtractor):
     _VALID_URL = r'https?://(?:(?:www|player)\.)?56\.com/(?:.+?/)?(?:v_|(?:play_album.+-))(?P<textid>.+?)\.(?:html|swf)'
     IE_NAME = '56.com'
     _TESTS = [{
+        'url': 'https://www.56.com/u76/v_MjAzMTkwNTY5.html',
+        'md5': '6cc69af917e34c1ff7d62e102b85027e',
+        'info_dict': {
+            'id': '735567287',
+            'ext': 'mp4',
+            'title': '焦点访谈丨多项重大工程进度条刷新 将这样改变你我生活',
+            'uploader': '央广网',
+            'duration': 350,
+            'timestamp': 1786058100,
+            'upload_date': '20260806',
+            'thumbnail': r're:https?://.+\.jpg',
+            'tags': ['焦点访谈', '工程', '新闻报道'],
+        },
+        'add_ie': ['Sohu'],
+    }, {
         'url': 'http://www.56.com/u39/v_OTM0NDA3MTY.html',
         'md5': 'e59995ac63d0457783ea05f93f12a866',
         'info_dict': {
@@ -14,6 +29,7 @@ class C56IE(InfoExtractor):
             'title': '网事知多少 第32期：车怒',
             'duration': 283.813,
         },
+        'skip': 'video gone',
     }, {
         'url': 'http://www.56.com/u47/v_MTM5NjQ5ODc2.html',
         'md5': '',
@@ -23,6 +39,7 @@ class C56IE(InfoExtractor):
         },
         'playlist_count': 7,
         'add_ie': ['Sohu'],
+        'skip': 'video gone',
     }]
 
     def _real_extract(self, url):
@@ -40,7 +57,9 @@ class C56IE(InfoExtractor):
         page = self._download_json(
             f'http://vxml.56.com/json/{text_id}/', text_id, 'Downloading video info')
 
-        info = page['info']
+        info = page.get('info')
+        if not info:
+            raise ExtractorError(page.get('msg') or 'Unable to extract video info', expected=True)
 
         formats = [
             {
