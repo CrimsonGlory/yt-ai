de3faebf2598aac5bd8e91963c3ac81c9de293fd
CrimsonGlory <javierbassi@gmail.com>
2026-08-26 21:19:40 +0000

    [audi-mediacenter.com] Fix live extractor test

--- yt_dlp/extractor/audimedia.py
+++ yt_dlp/extractor/audimedia.py
@@ -1,13 +1,33 @@
 from .common import InfoExtractor
 from ..utils import (
     int_or_none,
-    parse_iso8601,
+    str_or_none,
+    unified_strdate,
+    url_or_none,
 )
+from ..utils.traversal import require, traverse_obj
 
 
 class AudiMediaIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?audi-mediacenter\.com/(?:en|de)/audimediatv/(?:video/)?(?P<id>[^/?#]+)'
+    _VALID_URL = r'https?://(?:www\.)?(?:audi-mediacenter\.com|audi\.com)/(?P<lang>en|de)/(?:audimediatv(?:/video)?|videos/video)/(?P<id>[\w-]+)'
     _TESTS = [{
+        'url': 'https://www.audi-mediacenter.com/en/videos/video/audi-q7-suv-trailer-on-location-8422',
+        'md5': 'd39348d1948a2bbbb4132651033498da',
+        'info_dict': {
+            'id': '8422',
+            'ext': 'mp4',
+            'title': 'Audi Q7 SUV – Trailer (on location)',
+            'description': 'On location trailer of the Audi Q7 SUV from France including driving scenes as well as interior and exterior shots.',
+            'upload_date': '20260812',
+            'duration': 42,
+            'thumbnail': r're:https://uploads\.audi\.com/.+',
+            'tags': ['Audi Q7 SUV', 'design', 'exterior', 'interior', 'driving scenes'],
+        },
+        'params': {'format': 'http-720'},
+    }, {
+        'url': 'https://www.audi.com/en/videos/video/audi-q7-suv-trailer-on-location-8422',
+        'only_matching': True,
+    }, {
         'url': 'https://www.audi-mediacenter.com/en/audimediatv/60-seconds-of-audi-sport-104-2015-wec-bahrain-rookie-test-1467',
         'skip': 'video gone',
         'md5': '79a8b71c46d49042609795ab59779b66',
@@ -27,64 +47,37 @@ class AudiMediaIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        display_id = self._match_id(url)
-        webpage = self._download_webpage(url, display_id)
+        display_id, lang = self._match_valid_url(url).group('id', 'lang')
+        video_id = self._search_regex(r'(\d+)$', display_id, 'video id')
 
-        raw_payload = self._search_regex([
-            r'class="amtv-embed"[^>]+id="([0-9a-z-]+)"',
-            r'id="([0-9a-z-]+)"[^>]+class="amtv-embed"',
-            r'class=\\"amtv-embed\\"[^>]+id=\\"([0-9a-z-]+)\\"',
-            r'id=\\"([0-9a-z-]+)\\"[^>]+class=\\"amtv-embed\\"',
-            r'id=(?:\\)?"(amtve-[a-z]-\d+-[a-z]{2})',
-        ], webpage, 'raw payload')
-        _, stage_mode, video_id, _ = raw_payload.split('-')
+        video_data = traverse_obj(
+            self._download_json(
+                f'https://www.audimedia.tv/api/videos/{video_id}', video_id,
+                query={'locale': lang, 'id_type': 'amc'},
+                headers={'Accept': 'application/json'}),
+            ('data', {dict}, {require('video data')}))
 
-        # TODO: handle s and e stage_mode (live streams and ended live streams)
-        if stage_mode not in ('s', 'e'):
-            video_data = self._download_json(
-                'https://www.audimedia.tv/api/video/v1/videos/' + video_id,
-                video_id, query={
-                    'embed[]': ['video_versions', 'thumbnail_image'],
-                })['results']
-            formats = []
+        formats = []
+        hls_url = traverse_obj(video_data, ('stream_url_hls', {url_or_none}))
+        if hls_url:
+            formats.extend(self._extract_m3u8_formats(
+                hls_url, video_id, 'mp4', m3u8_id='hls', fatal=False))
+        formats.extend(traverse_obj(video_data, ('downloads', lambda _, v: url_or_none(v['url']), {
+            'url': 'url',
+            'height': ('height', {int_or_none}),
+            'format_id': ('height', {int_or_none}, {lambda h: f'http-{h}'}),
+        })))
 
-            stream_url_hls = video_data.get('stream_url_hls')
-            if stream_url_hls:
-                formats.extend(self._extract_m3u8_formats(
-                    stream_url_hls, video_id, 'mp4',
-                    entry_protocol='m3u8_native', m3u8_id='hls', fatal=False))
-
-            stream_url_hds = video_data.get('stream_url_hds')
-            if stream_url_hds:
-                formats.extend(self._extract_f4m_formats(
-                    stream_url_hds + '?hdcore=3.4.0',
-                    video_id, f4m_id='hds', fatal=False))
-
-            for video_version in video_data.get('video_versions', []):
-                video_version_url = video_version.get('download_url') or video_version.get('stream_url')
-                if not video_version_url:
-                    continue
-                f = {
-                    'url': video_version_url,
-                    'width': int_or_none(video_version.get('width')),
-                    'height': int_or_none(video_version.get('height')),
-                    'abr': int_or_none(video_version.get('audio_bitrate')),
-                    'vbr': int_or_none(video_version.get('video_bitrate')),
-                }
-                bitrate = self._search_regex(r'(\d+)k', video_version_url, 'bitrate', default=None)
-                if bitrate:
-                    f.update({
-                        'format_id': f'http-{bitrate}',
-                    })
-                formats.append(f)
-
-            return {
-                'id': video_id,
-                'title': video_data['title'],
-                'description': video_data.get('subtitle'),
-                'thumbnail': video_data.get('thumbnail_image', {}).get('file'),
-                'timestamp': parse_iso8601(video_data.get('publication_date')),
-                'duration': int_or_none(video_data.get('duration')),
-                'view_count': int_or_none(video_data.get('view_count')),
-                'formats': formats,
-            }
+        return {
+            'id': video_id,
+            'formats': formats,
+            **traverse_obj(video_data, {
+                'id': ('id', {int}, {str_or_none}),
+                'title': ('title', {str}),
+                'description': ('description_text', {str}),
+                'thumbnail': ('splash_image_url', {url_or_none}),
+                'upload_date': ('public_date', {unified_strdate}),
+                'duration': ('duration_millis', {int_or_none(scale=1000)}),
+                'tags': ('tags', ..., {str}),
+            }),
+        }
