39ccbfe7f2b49b51f851d21e53f653b8aa393d73
CrimsonGlory <javierbassi@gmail.com>
2026-08-26 21:42:07 +0000

    [beatport.com] Fix live extractor test

--- yt_dlp/extractor/beatport.py
+++ yt_dlp/extractor/beatport.py
@@ -1,99 +1,86 @@
-import re
-
 from .common import InfoExtractor
-from ..utils import int_or_none
+from ..utils import (
+    float_or_none,
+    traverse_obj,
+    unified_timestamp,
+    url_or_none,
+)
 
 
 class BeatportIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.|pro\.)?beatport\.com/track/(?P<display_id>[^/]+)/(?P<id>[0-9]+)'
+    _VALID_URL = r'https?://(?:www\.|pro\.)?beatport\.com/track/(?P<display_id>[^/?#]+)/(?P<id>\d+)'
     _TESTS = [{
         'url': 'https://beatport.com/track/synesthesia-original-mix/5379371',
-        'skip': 'HTTP Error 403',
-        'md5': 'b3c34d8639a2f6a7f734382358478887',
+        'md5': 'cfcc245aafcad52a837b2c5a60a472c9',
         'info_dict': {
             'id': '5379371',
             'display_id': 'synesthesia-original-mix',
-            'ext': 'mp4',
+            'ext': 'mp3',
             'title': 'Froxic - Synesthesia (Original Mix)',
+            'track': 'Synesthesia',
+            'artists': ['Froxic'],
+            'album': 'Synesthesia',
+            'duration': 337.968,
+            'timestamp': 1398643200,
+            'upload_date': '20140428',
+            'thumbnail': r're:https://geo-media\.beatport\.com/.+',
+            'genres': ['Mainstage'],
         },
     }, {
         'url': 'https://beatport.com/track/love-and-war-original-mix/3756896',
-        'skip': 'HTTP Error 403',
-        'md5': 'e44c3025dfa38c6577fbaeb43da43514',
-        'info_dict': {
-            'id': '3756896',
-            'display_id': 'love-and-war-original-mix',
-            'ext': 'mp3',
-            'title': 'Wolfgang Gartner - Love & War (Original Mix)',
-        },
+        'only_matching': True,
     }, {
         'url': 'https://beatport.com/track/birds-original-mix/4991738',
-        'skip': 'HTTP Error 403',
-        'md5': 'a1fd8e8046de3950fd039304c186c05f',
-        'info_dict': {
-            'id': '4991738',
-            'display_id': 'birds-original-mix',
-            'ext': 'mp4',
-            'title': "Tos, Middle Milk, Mumblin' Johnsson - Birds (Original Mix)",
-        },
+        'only_matching': True,
     }]
 
     def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        track_id = mobj.group('id')
-        display_id = mobj.group('display_id')
+        display_id, track_id = self._match_valid_url(url).group('display_id', 'id')
+        webpage = self._download_webpage(url, display_id, impersonate=True)
+        track = traverse_obj(
+            self._search_nextjs_data(webpage, track_id),
+            ('props', 'pageProps', 'track', {dict}))
+        if not track:
+            self.raise_no_formats('Unable to extract track metadata', video_id=track_id)
 
-        webpage = self._download_webpage(url, display_id)
+        artists = traverse_obj(track, ('artists', ..., 'name', {str}))
+        title = track['name']
+        if artists:
+            title = f'{", ".join(artists)} - {title}'
+        mix = traverse_obj(track, ('mix_name', {str}))
+        if mix:
+            title = f'{title} ({mix})'
 
-        playables = self._parse_json(
-            self._search_regex(
-                r'window\.Playables\s*=\s*({.+?});', webpage,
-                'playables info', flags=re.DOTALL),
-            track_id)
+        sample_url = traverse_obj(track, ('sample_url', {url_or_none}))
+        if not sample_url:
+            self.raise_no_formats('No preview is available for this track', expected=True, video_id=track_id)
 
-        track = next(t for t in playables['tracks'] if t['id'] == int(track_id))
-
-        title = ', '.join(a['name'] for a in track['artists']) + ' - ' + track['name']
-        if track['mix']:
-            title += ' (' + track['mix'] + ')'
-
-        formats = []
-        for ext, info in track['preview'].items():
-            if not info['url']:
-                continue
-            fmt = {
-                'url': info['url'],
-                'ext': ext,
-                'format_id': ext,
-                'vcodec': 'none',
-            }
-            if ext == 'mp3':
-                fmt['acodec'] = 'mp3'
-                fmt['abr'] = 96
-                fmt['asr'] = 44100
-            elif ext == 'mp4':
-                fmt['acodec'] = 'aac'
-                fmt['abr'] = 96
-                fmt['asr'] = 44100
-            formats.append(fmt)
-
-        images = []
-        for name, info in track['images'].items():
-            image_url = info.get('url')
-            if name == 'dynamic' or not image_url:
-                continue
-            image = {
-                'id': name,
-                'url': image_url,
-                'height': int_or_none(info.get('height')),
-                'width': int_or_none(info.get('width')),
-            }
-            images.append(image)
+        thumbnails, seen = [], set()
+        for thumb_id, path in (('image', ('image', 'uri')), ('cover', ('release', 'image', 'uri'))):
+            image_url = traverse_obj(track, (*path, {url_or_none}))
+            if image_url and image_url not in seen:
+                seen.add(image_url)
+                thumbnails.append({
+                    'id': thumb_id,
+                    'url': image_url,
+                })
 
         return {
-            'id': str(track.get('id')) or track_id,
-            'display_id': track.get('slug') or display_id,
+            'id': track_id,
+            'display_id': display_id,
             'title': title,
-            'formats': formats,
-            'thumbnails': images,
+            'track': traverse_obj(track, ('name', {str})),
+            'artists': artists or None,
+            'album': traverse_obj(track, ('release', 'name', {str})),
+            'duration': float_or_none(track.get('length_ms'), scale=1000),
+            'timestamp': unified_timestamp(track.get('publish_date')),
+            'thumbnails': thumbnails,
+            'genres': traverse_obj(track, ('genre', 'name', {str}, filter, all)),
+            'formats': [{
+                'url': sample_url,
+                'format_id': 'sample',
+                'ext': 'mp3',
+                'vcodec': 'none',
+                'acodec': 'mp3',
+            }],
         }
