baa90b9704397566888e7074b36ce50c9310d27d
CrimsonGlory <javierbassi@gmail.com>
2026-08-26 18:57:28 +0000

    [alphaporno.com] Fix live extractor test

--- yt_dlp/extractor/alphaporno.py
+++ yt_dlp/extractor/alphaporno.py
@@ -1,32 +1,33 @@
 from .common import InfoExtractor
 from ..utils import (
     int_or_none,
+    merge_dicts,
     parse_duration,
-    parse_filesize,
     parse_iso8601,
+    remove_end,
+    unified_timestamp,
 )
 
 
 class AlphaPornoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?alphaporno\.com/videos/(?P<id>[^/]+)'
-    _TEST = {
+    _TESTS = [{
         'url': 'http://www.alphaporno.com/videos/sensual-striptease-porn-with-samantha-alexandra/',
-        'md5': 'feb6d3bba8848cd54467a87ad34bd38e',
+        'md5': '7e6a1cdd48fa67362a5a11d7039164e7',
         'info_dict': {
             'id': '258807',
             'display_id': 'sensual-striptease-porn-with-samantha-alexandra',
             'ext': 'mp4',
             'title': 'Sensual striptease porn with Samantha Alexandra',
             'thumbnail': r're:https?://.*\.jpg$',
-            'timestamp': 1418694611,
+            'timestamp': 1418701811,
             'upload_date': '20141216',
             'duration': 387,
-            'filesize_approx': 54120000,
-            'tbr': 1145,
+            'view_count': int,
             'categories': list,
             'age_limit': 18,
         },
-    }
+    }]
 
     def _real_extract(self, url):
         display_id = self._match_id(url)
@@ -34,42 +35,42 @@ class AlphaPornoIE(InfoExtractor):
         webpage = self._download_webpage(url, display_id)
 
         video_id = self._search_regex(
-            r"video_id\s*:\s*'([^']+)'", webpage, 'video id', default=None)
+            (r'chs_object\s*=\s*[\'"](\d+)',
+             r'params\[[\'"]video_id[\'"]\]\s*=\s*(\d+)',
+             r'video_id\s*[:=]\s*[\'"]?(\d+)'),
+            webpage, 'video id', default=display_id)
 
-        video_url = self._search_regex(
-            r"video_url\s*:\s*'([^']+)'", webpage, 'video url')
-        ext = self._html_search_meta(
-            'encodingFormat', webpage, 'ext', default='.mp4')[1:]
+        title = remove_end(self._html_extract_title(webpage), ' - Alpha Porno')
 
-        title = self._search_regex(
-            [r'<meta content="([^"]+)" itemprop="description">',
-             r'class="title" itemprop="name">([^<]+)<'],
-            webpage, 'title')
-        thumbnail = self._html_search_meta('thumbnail', webpage, 'thumbnail')
-        timestamp = parse_iso8601(self._html_search_meta(
-            'uploadDate', webpage, 'upload date'))
-        duration = parse_duration(self._html_search_meta(
-            'duration', webpage, 'duration'))
-        filesize_approx = parse_filesize(self._html_search_meta(
-            'contentSize', webpage, 'file size'))
-        bitrate = int_or_none(self._html_search_meta(
-            'bitrate', webpage, 'bitrate'))
-        categories = self._html_search_meta(
-            'keywords', webpage, 'categories', default='').split(',')
+        media = self._parse_html5_media_entries(url, webpage, display_id)
+        info = media[0] if media else {}
+        if not info.get('formats') and not info.get('url'):
+            info['url'] = self._search_regex(
+                r"video_url\s*:\s*'([^']+)'", webpage, 'video url')
 
-        age_limit = self._rta_search(webpage)
+        categories = [
+            c.strip()
+            for c in self._html_search_meta(
+                'keywords', webpage, 'categories', default='').split(',')
+            if c.strip()]
+        duration = int_or_none(self._og_search_property(
+            'video:duration', webpage, default=None)) or parse_duration(
+            self._html_search_meta('duration', webpage, default=None))
+        timestamp = unified_timestamp(self._og_search_property(
+            'video:release_date', webpage, default=None)) or parse_iso8601(
+            self._html_search_meta('uploadDate', webpage, default=None))
+        view_count = int_or_none(self._search_regex(
+            r'<span[^>]+class=["\']views["\'][^>]*>(\d+)', webpage,
+            'view count', default=None))
 
-        return {
+        return merge_dicts(info, {
             'id': video_id,
             'display_id': display_id,
-            'url': video_url,
-            'ext': ext,
             'title': title,
-            'thumbnail': thumbnail,
-            'timestamp': timestamp,
-            'duration': duration,
-            'filesize_approx': filesize_approx,
-            'tbr': bitrate,
+            'thumbnail': self._og_search_thumbnail(webpage),
             'categories': categories,
-            'age_limit': age_limit,
-        }
+            'duration': duration,
+            'timestamp': timestamp,
+            'view_count': view_count,
+            'age_limit': self._rta_search(webpage) or 18,
+        })
