1b9695446a3465a4f86cb78c2b3b59c3878bd13a
CrimsonGlory <javierbassi@gmail.com>
2026-08-26 22:11:27 +0000

    [bfi.org.uk] Fix live extractor test

--- yt_dlp/extractor/bfi.py
+++ yt_dlp/extractor/bfi.py
@@ -1,35 +1,64 @@
 import re
 
+from .brightcove import BrightcoveNewIE
 from .common import InfoExtractor
-from ..utils import extract_attributes
+from ..utils import (
+    ExtractorError,
+    extract_attributes,
+    smuggle_url,
+)
 
 
 class BFIPlayerIE(InfoExtractor):
     _WEB_FALLBACK = True
     IE_NAME = 'bfi:player'
     _VALID_URL = r'https?://player\.bfi\.org\.uk/[^/]+/film/watch-(?P<id>[\w-]+)-online'
-    _TEST = {
-        'url': 'https://player.bfi.org.uk/free/film/watch-computer-doctor-1974-online',
-        'md5': 'e8783ebd8e061ec4bc6e9501ed547de8',
+    _TESTS = [{
+        'url': 'https://player.bfi.org.uk/free/film/watch-callum-turners-3-you-must-see-on-bfi-player-2026-online',
+        'md5': '8bee4f86b6ad696b389fca2106b939e5',
         'info_dict': {
-            'id': 'htNnhlZjE60C9VySkQEIBtU-cNV1Xx63',
+            'id': '6399173210112',
             'ext': 'mp4',
-            'title': 'Computer Doctor',
-            'description': 'md5:fb6c240d40c4dbe40428bdd62f78203b',
+            'title': "Callum Turner's 3 You Must See on BFI Player",
+            'uploader_id': '6057949427001',
+            'duration': 117.931,
+            'timestamp': 1782224497,
+            'upload_date': '20260623',
+            'tags': ['free', 'archived'],
+            'thumbnail': r're:https?://.+\.jpg',
         },
-        'skip': 'BFI Player films cannot be played outside of the UK',
-    }
+        'add_ie': ['BrightcoveNew'],
+    }, {
+        'url': 'https://player.bfi.org.uk/free/film/watch-computer-doctor-1974-online',
+        'info_dict': {
+            'id': 'computer-doctor-1974',
+            'ext': 'mp4',
+        },
+        'skip': 'This film has moved to BFI Replay',
+    }, {
+        'url': 'https://player.bfi.org.uk/rentals/film/watch-my-fathers-island-2025-online',
+        'only_matching': True,
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
         entries = []
-        for player_el in re.findall(r'(?s)<[^>]+class="player"[^>]*>', webpage):
-            player_attr = extract_attributes(player_el)
-            ooyala_id = player_attr.get('data-video-id')
-            if not ooyala_id:
+        for player_el in re.findall(r'(?s)<video-js[^>]*>', webpage):
+            attrs = extract_attributes(player_el)
+            bc_id = attrs.get('data-video-id') or attrs.get('data-ref-id')
+            account_id = attrs.get('data-account') or attrs.get('data-acid')
+            if not bc_id or not account_id:
                 continue
+            player_id = attrs.get('data-player') or attrs.get('data-pid') or 'default'
+            embed = attrs.get('data-embed') or 'default'
             entries.append(self.url_result(
-                'ooyala:' + ooyala_id, 'Ooyala',
-                ooyala_id, player_attr.get('data-label')))
-        return self.playlist_result(entries)
+                smuggle_url(
+                    f'https://players.brightcove.net/{account_id}/{player_id}_{embed}/index.html?videoId={bc_id}',
+                    {'referrer': url, 'geo_countries': ['GB']}),
+                BrightcoveNewIE, bc_id, attrs.get('data-label')))
+        if not entries:
+            raise ExtractorError('Unable to find Brightcove player', expected=True)
+        if len(entries) == 1:
+            return entries[0]
+        return self.playlist_result(entries, video_id)
