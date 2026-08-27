cc4e8ebc44f8d6fb8aa59ac330a1490b476a4f42
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 12:53:48 +0000

    [hotnewhiphop.com] Fix live extractor test

--- yt_dlp/extractor/hotnewhiphop.py
+++ yt_dlp/extractor/hotnewhiphop.py
@@ -1,14 +1,51 @@
-import base64
-
 from .common import InfoExtractor
-from ..networking import HEADRequest, Request
-from ..utils import ExtractorError, urlencode_postdata
+from .youtube import YoutubeIE
+from ..utils import ExtractorError
 
 
 class HotNewHipHopIE(InfoExtractor):
     _WEB_FALLBACK = True
-    _VALID_URL = r'https?://(?:www\.)?hotnewhiphop\.com/.*\.(?P<id>.*)\.html'
-    _TEST = {
+    _VALID_URL = r'https?://(?:www\.)?hotnewhiphop\.com/(?:[^/?#]*\.)?(?P<id>\d+)(?:-[^/?#]+)?(?:\.html)?/?(?:[?#]|$)'
+    _TESTS = [{
+        'url': 'https://www.hotnewhiphop.com/964555-kanye-west-comeback-hnhh-tv',
+        'md5': '80df74f111a471584698322a011e4f3f',
+        'info_dict': {
+            'id': '80_XD4sYxfo',
+            'ext': 'mp4',
+            'title': 'Can Kanye West Make A Comeback?',
+            'description': 'md5:916d632450ca23f23115ff3752057c84',
+            'media_type': 'video',
+            'uploader': 'HotNewHipHop',
+            'uploader_id': '@HotNewHipHop',
+            'uploader_url': 'https://www.youtube.com/@HotNewHipHop',
+            'channel': 'HotNewHipHop',
+            'channel_id': 'UCKaDN209nOLUabc-ftR_lng',
+            'channel_url': 'https://www.youtube.com/channel/UCKaDN209nOLUabc-ftR_lng',
+            'channel_is_verified': True,
+            'channel_follower_count': int,
+            'comment_count': int,
+            'view_count': int,
+            'like_count': int,
+            'age_limit': 0,
+            'duration': 509,
+            'thumbnail': r're:https?://i\.ytimg\.com/vi/.+',
+            'categories': ['Music'],
+            'tags': 'count:24',
+            'timestamp': 1765474699,
+            'upload_date': '20251211',
+            'playable_in_embed': True,
+            'availability': 'public',
+            'live_status': 'not_live',
+        },
+        'params': {
+            'format': 'bestvideo[protocol=https][ext=mp4]/best[protocol=https]',
+        },
+        'add_ie': ['Youtube'],
+        'expected_warnings': [
+            'Remote component challenge solver script',
+            'No supported JavaScript runtime',
+        ],
+    }, {
         'url': 'http://www.hotnewhiphop.com/freddie-gibbs-lay-it-down-song.1435540.html',
         'skip': 'video gone',
         'md5': '2c2cd2f76ef11a9b3b581e8b232f3d96',
@@ -17,47 +54,21 @@ class HotNewHipHopIE(InfoExtractor):
             'ext': 'mp3',
             'title': 'Freddie Gibbs - Lay It Down',
         },
-    }
+    }, {
+        'url': 'https://www.hotnewhiphop.com/1007296-zilla-westside-gunn',
+        'only_matching': True,
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        video_url_base64 = self._search_regex(
-            r'data-path="(.*?)"', webpage, 'video URL', default=None)
+        youtube_id = self._search_regex(
+            (r'<amp-youtube[^>]+\bdata-videoid=["\']([0-9A-Za-z_-]{11})',
+             r'(?:youtube(?:-nocookie)?\.com/(?:watch\?v=|embed/)|youtu\.be/)([0-9A-Za-z_-]{11})'),
+            webpage, 'youtube id', default=None)
+        if not youtube_id:
+            raise ExtractorError('No video found', expected=True)
 
-        if video_url_base64 is None:
-            video_url = self._search_regex(
-                r'"contentUrl" content="(.*?)"', webpage, 'content URL')
-            return self.url_result(video_url, ie='Youtube')
-
-        reqdata = urlencode_postdata([
-            ('mediaType', 's'),
-            ('mediaId', video_id),
-        ])
-        r = Request(
-            'http://www.hotnewhiphop.com/ajax/media/getActions/', data=reqdata)
-        r.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        mkd = self._download_json(
-            r, video_id, note='Requesting media key',
-            errnote='Could not download media key')
-        if 'mediaKey' not in mkd:
-            raise ExtractorError('Did not get a media key')
-
-        redirect_url = base64.b64decode(video_url_base64).decode('utf-8')
-        redirect_req = HEADRequest(redirect_url)
-        req = self._request_webpage(
-            redirect_req, video_id,
-            note='Resolving final URL', errnote='Could not resolve final URL')
-        video_url = req.url
-        if video_url.endswith('.html'):
-            raise ExtractorError('Redirect failed')
-
-        video_title = self._og_search_title(webpage).strip()
-
-        return {
-            'id': video_id,
-            'url': video_url,
-            'title': video_title,
-            'thumbnail': self._og_search_thumbnail(webpage),
-        }
+        return self.url_result(
+            f'https://www.youtube.com/watch?v={youtube_id}', YoutubeIE, youtube_id)
