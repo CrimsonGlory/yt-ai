83be4e0d006f7f28a4b167cde6cd3fb30feef4c4
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 05:30:32 +0000

    [dhm.de] Fix live extractor test

--- yt_dlp/extractor/dhm.py
+++ yt_dlp/extractor/dhm.py
@@ -1,13 +1,25 @@
+import re
+
 from .common import InfoExtractor
-from ..utils import parse_duration
+from ..utils import parse_duration, url_basename, urljoin
 
 
 class DHMIE(InfoExtractor):
     _WEB_FALLBACK = True
-    IE_DESC = 'Filmarchiv - Deutsches Historisches Museum'
-    _VALID_URL = r'https?://(?:www\.)?dhm\.de/filmarchiv/(?:[^/]+/)+(?P<id>[^/]+)'
+    IE_DESC = 'Deutsches Historisches Museum'
+    _VALID_URL = r'https?://(?:www\.)?dhm\.de/(?:filmarchiv/(?:[^/?#]+/)+|journal/beitrag/)(?P<id>[^/?#]+)/?'
 
     _TESTS = [{
+        'url': 'https://www.dhm.de/journal/beitrag/man-konnte-ploetzlich-nur-noch-unter-lebensgefahr-dieses-land-verlassen-renate-werwigk-schneider-erinnert-sich-an-den-bau-der-berliner-mauer',
+        'md5': '59df5e5aff667d3840e29732a2054844',
+        'info_dict': {
+            'id': 'C20260211_1062',
+            'ext': 'mp4',
+            'display_id': 'man-konnte-ploetzlich-nur-noch-unter-lebensgefahr-dieses-land-verlassen-renate-werwigk-schneider-erinnert-sich-an-den-bau-der-berliner-mauer',
+            'title': '„Man konnte plötzlich nur noch unter Lebensgefahr dieses Land verlassen“ – Renate Werwigk-Schneider erinnert sich an den Bau der Berliner Mauer',
+            'thumbnail': r're:https?://www\.dhm\.de/journal/.+\.(?:webp|jpg|jpeg|png)',
+        },
+    }, {
         'url': 'http://www.dhm.de/filmarchiv/die-filme/the-marshallplan-at-work-in-west-germany/',
         'skip': 'video gone',
         'md5': '11c475f670209bf6acca0b2b7ef51827',
@@ -29,32 +41,48 @@ class DHMIE(InfoExtractor):
             'title': 'ROLLE 1',
             'thumbnail': r're:^https?://.*\.jpg$',
         },
+    }, {
+        'url': 'https://www.dhm.de/journal/beitrag/vergleichsgeschichte-zweier-gewaltregime-kurator-stephan-malinowski-ueber-die-kommende-ausstellung-umstrittene-verwandtschaft-koloniale-und-nationalsozialistische-gewalt',
+        'only_matching': True,
     }]
 
     def _real_extract(self, url):
-        playlist_id = self._match_id(url)
-
-        webpage = self._download_webpage(url, playlist_id)
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
 
         playlist_url = self._search_regex(
-            r"file\s*:\s*'([^']+)'", webpage, 'playlist url')
+            r"file\s*:\s*'([^']+)'", webpage, 'playlist url', default=None)
+        if playlist_url:
+            entries = self._extract_xspf_playlist(playlist_url, display_id)
 
-        entries = self._extract_xspf_playlist(playlist_url, playlist_id)
+            title = self._search_regex(
+                [r'dc:title="([^"]+)"', r'<title> &raquo;([^<]+)</title>'],
+                webpage, 'title').strip()
+            description = self._html_search_regex(
+                r'<p><strong>Description:</strong>(.+?)</p>',
+                webpage, 'description', default=None)
+            duration = parse_duration(self._search_regex(
+                r'<em>Length\s*</em>\s*:\s*</strong>([^<]+)',
+                webpage, 'duration', default=None))
 
-        title = self._search_regex(
-            [r'dc:title="([^"]+)"', r'<title> &raquo;([^<]+)</title>'],
-            webpage, 'title').strip()
-        description = self._html_search_regex(
-            r'<p><strong>Description:</strong>(.+?)</p>',
-            webpage, 'description', default=None)
-        duration = parse_duration(self._search_regex(
-            r'<em>Length\s*</em>\s*:\s*</strong>([^<]+)',
-            webpage, 'duration', default=None))
+            entries[0].update({
+                'title': title,
+                'description': description,
+                'duration': duration,
+            })
 
-        entries[0].update({
-            'title': title,
-            'description': description,
-            'duration': duration,
-        })
+            return self.playlist_result(entries, display_id)
 
-        return self.playlist_result(entries, playlist_id)
+        media_url = urljoin(url, self._search_regex(
+            r'data-src=(["\'])(?P<url>(?:(?!\1).)+\.(?:mp4|m4a|webm|mp3)(?:\?(?:(?!\1).)*)?)\1',
+            webpage, 'media url', group='url'))
+
+        return {
+            'id': url_basename(media_url).rsplit('.', 1)[0],
+            'display_id': display_id,
+            'url': media_url,
+            'title': self._og_search_title(webpage) or self._html_extract_title(webpage),
+            'thumbnail': urljoin(url, self._search_regex(
+                r'data-src=["\'][^"\']+\.(?:mp4|m4a|webm|mp3)(?:\?[^"\']*)?["\'][^>]*>\s*<img[^>]+src=(["\'])(?P<url>(?:(?!\1).)+)\1',
+                webpage, 'thumbnail', default=None, group='url', flags=re.DOTALL)),
+        }
