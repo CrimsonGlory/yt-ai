ef4bf9fd73b8b2b5a15518a88cb77141307fc1f4
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 10:51:42 +0000

    [gamestar.de] Fix live extractor test

--- yt_dlp/extractor/gamestar.py
+++ yt_dlp/extractor/gamestar.py
@@ -1,61 +1,50 @@
 from .common import InfoExtractor
-from ..utils import (
-    int_or_none,
-    remove_end,
-)
+from .dailymotion import DailymotionIE
+from ..utils.traversal import require, traverse_obj
 
 
 class GameStarIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?game(?P<site>pro|star)\.de/videos/.*,(?P<id>[0-9]+)\.html'
     _TESTS = [{
         'url': 'http://www.gamestar.de/videos/trailer,3/hobbit-3-die-schlacht-der-fuenf-heere,76110.html',
-        'skip': 'HTTP Error 403',
-        'md5': 'ee782f1f8050448c95c5cacd63bc851c',
+        'md5': '3a1b2d7494e6f3230a6835038436d8ac',
         'info_dict': {
-            'id': '76110',
+            'id': 'k4b9wq1ir4jtBxy5fgF',
             'ext': 'mp4',
             'title': 'Hobbit 3: Die Schlacht der Fünf Heere - Teaser-Trailer zum dritten Teil',
-            'description': 'Der Teaser-Trailer zu Hobbit 3: Die Schlacht der Fünf Heere zeigt einige Szenen aus dem dritten Teil der Saga und kündigt den...',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'timestamp': 1406542380,
-            'upload_date': '20140728',
+            'description': 'md5:d543730d27e0dc4e839cee9d219f42a8',
+            'thumbnail': r're:https?://s\d+\.dmcdn\.net/v/',
             'duration': 17,
+            'timestamp': 1655790897,
+            'upload_date': '20220621',
+            'uploader': 'GameStar',
+            'uploader_id': 'x2mvqg3',
+            'age_limit': 0,
+            'view_count': int,
+            'like_count': int,
+            'tags': ['Trailer', 'Video'],
         },
+        'params': {'format': 'hls-720'},
+        'add_ie': ['Dailymotion'],
     }, {
         'url': 'http://www.gamepro.de/videos/top-10-indie-spiele-fuer-nintendo-switch-video-tolle-nindies-games-zum-download,95316.html',
         'only_matching': True,
     }, {
         'url': 'http://www.gamestar.de/videos/top-10-indie-spiele-fuer-nintendo-switch-video-tolle-nindies-games-zum-download,95316.html',
         'only_matching': True,
+    }, {
+        'url': 'https://www.gamestar.de/videos/im-neuen-gameplay-trailer-von-the-expanse-osiris-reborn-spruehen-die-funken,141896.html',
+        'only_matching': True,
     }]
 
     def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        site = mobj.group('site')
-        video_id = mobj.group('id')
+        video_id = self._match_id(url)
+        webpage = self._download_webpage(url, video_id, impersonate='firefox')
 
-        webpage = self._download_webpage(url, video_id)
+        player = self._search_json(
+            r'setupVideoPlayer\(', webpage, 'player config', video_id)
+        dm_id = traverse_obj(player, ('player', 'dmId', {str}, {require('dailymotion id')}))
 
-        # TODO: there are multiple ld+json objects in the webpage,
-        # while _search_json_ld finds only the first one
-        json_ld = self._parse_json(self._search_regex(
-            r'(?s)<script[^>]+type=(["\'])application/ld\+json\1[^>]*>(?P<json_ld>[^<]+VideoObject[^<]+)</script>',
-            webpage, 'JSON-LD', group='json_ld'), video_id)
-        info_dict = self._json_ld(json_ld, video_id)
-        info_dict['title'] = remove_end(
-            info_dict['title'], f' - Game{site.title()}')
-
-        view_count = int_or_none(json_ld.get('interactionCount'))
-        comment_count = int_or_none(self._html_search_regex(
-            r'<span>Kommentare</span>\s*<span[^>]+class=["\']count[^>]+>\s*\(\s*([0-9]+)',
-            webpage, 'comment count', fatal=False))
-
-        info_dict.update({
-            'id': video_id,
-            'url': 'http://gamestar.de/_misc/videos/portal/getVideoUrl.cfm?premium=0&videoId=' + video_id,
-            'ext': 'mp4',
-            'view_count': view_count,
-            'comment_count': comment_count,
-        })
-
-        return info_dict
+        return self.url_result(
+            f'https://www.dailymotion.com/video/{dm_id}',
+            ie=DailymotionIE, video_id=dm_id)
