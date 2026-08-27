bfb99bae30bb8ec919d8d73a465825c93fd386c2
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 21:55:05 +0000

    [metacritic.com] Fix live extractor test

--- yt_dlp/extractor/metacritic.py
+++ yt_dlp/extractor/metacritic.py
@@ -1,15 +1,31 @@
-import re
-
 from .common import InfoExtractor
-from ..utils import (
-    fix_xml_ampersands,
-)
+from .jwplatform import JWPlatformIE
 
 
 class MetacriticIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?metacritic\.com/.+?/trailers/(?P<id>\d+)'
-
+    _VALID_URL = (
+        r'https?://(?:www\.)?metacritic\.com/(?:movie|game|tv)/(?P<id>[^/?#]+)/?(?:[?#]|$)',
+        r'https?://(?:www\.)?metacritic\.com/.+?/trailers/(?P<id>\d+)',
+    )
     _TESTS = [{
+        'url': 'https://www.metacritic.com/movie/the-godfather/',
+        'md5': 'e3f89a3ae06f8c723873355fa2dffb17',
+        'info_dict': {
+            'id': 'zIUMZ9F8',
+            'ext': 'mp4',
+            'title': 'The Godfather (Accolades Trailer)',
+            'display_id': 'the-godfather',
+            'description': '',
+            'duration': 58.0,
+            'thumbnail': r're:https?://cdn\.jwplayer\.com/v2/media/.+',
+            'timestamp': 1751260213,
+            'upload_date': '20250630',
+        },
+        'params': {
+            # Prefer progressive MP4 so the live test is not HLS-only
+            'format': 'best[protocol=https][ext=mp4]/best',
+        },
+    }, {
         'url': 'http://www.metacritic.com/game/playstation-4/infamous-second-son/trailers/3698222',
         'info_dict': {
             'id': '3698222',
@@ -18,7 +34,7 @@ class MetacriticIE(InfoExtractor):
             'description': 'Take a peak behind-the-scenes to see how Sucker Punch brings smoke into the universe of inFAMOUS Second Son on the PS4.',
             'duration': 221,
         },
-        'skip': 'Not providing trailers anymore',
+        'skip': 'Old trailer URLs no longer serve the original clips',
     }, {
         'url': 'http://www.metacritic.com/game/playstation-4/tales-from-the-borderlands-a-telltale-game-series/trailers/5740315',
         'skip': 'video gone',
@@ -29,35 +45,20 @@ class MetacriticIE(InfoExtractor):
             'description': 'In the final episode of the season, all hell breaks loose. Jack is now in control of Helios\' systems, and he\'s ready to reclaim his rightful place as king of Hyperion (with or without you).',
             'duration': 114,
         },
+    }, {
+        'url': 'https://www.metacritic.com/game/elden-ring/',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.metacritic.com/tv/the-pitt/',
+        'only_matching': True,
     }]
 
     def _real_extract(self, url):
-        mobj = self._match_valid_url(url)
-        video_id = mobj.group('id')
-        webpage = self._download_webpage(url, video_id)
-        # The xml is not well formatted, there are raw '&'
-        info = self._download_xml('http://www.metacritic.com/video_data?video=' + video_id,
-                                  video_id, 'Downloading info xml', transform_source=fix_xml_ampersands)
-
-        clip = next(c for c in info.findall('playList/clip') if c.find('id').text == video_id)
-        formats = []
-        for video_file in clip.findall('httpURI/videoFile'):
-            rate_str = video_file.find('rate').text
-            video_url = video_file.find('filePath').text
-            formats.append({
-                'url': video_url,
-                'ext': 'mp4',
-                'format_id': rate_str,
-                'tbr': int(rate_str),
-            })
-
-        description = self._html_search_regex(r'<b>Description:</b>(.*?)</p>',
-                                              webpage, 'description', flags=re.DOTALL)
-
-        return {
-            'id': video_id,
-            'title': clip.find('title').text,
-            'formats': formats,
-            'description': description,
-            'duration': int(clip.find('duration').text),
-        }
+        display_id = self._match_id(url)
+        webpage = self._download_webpage(url, display_id)
+        jw_id = self._search_regex(
+            r'https?://cdn\.jwplayer\.com/manifests/([a-zA-Z0-9]{8})\.m3u8',
+            webpage, 'jwplayer id')
+        return self.url_result(
+            f'jwplatform:{jw_id}', JWPlatformIE, jw_id,
+            url_transparent=True, display_id=display_id)
