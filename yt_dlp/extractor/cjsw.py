642822b67feb8e1dc4ed43530bfdd30e0b7d5749
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 02:09:41 +0000

    [cjsw.com] Fix live extractor test

--- yt_dlp/extractor/cjsw.py
+++ yt_dlp/extractor/cjsw.py
@@ -8,7 +8,19 @@ from ..utils import (
 class CJSWIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?cjsw\.com/program/(?P<program>[^/]+)/episode/(?P<id>\d+)'
     _TESTS = [{
+        'url': 'https://cjsw.com/program/the-sugar-bowl/episode/20260825',
+        'md5': '0c031099a0a83c48b376e8fbb9b510a6',
+        'info_dict': {
+            'id': '1d9b9229-e7cb-472f-ab43-6ea53a83b24a',
+            'ext': 'mp3',
+            'title': 'The Sugar Bowl – Episode August 25, 2026',
+            'description': "Rest in peace to the queen of country, and America's sweetheart Dolly Parton!",
+            'series': 'The Sugar Bowl',
+            'episode_id': '20260825',
+        },
+    }, {
         'url': 'http://cjsw.com/program/freshly-squeezed/episode/20170620',
+        'skip': 'audio gone',
         'md5': 'cee14d40f1e9433632c56e3d14977120',
         'info_dict': {
             'id': '91d9f016-a2e7-46c5-8dcb-7cbcd7437c41',
