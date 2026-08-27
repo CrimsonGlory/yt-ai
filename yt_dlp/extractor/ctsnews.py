9072e45ad3a53ffdf6d383d80c121943dea691d5
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 03:06:54 +0000

    [cts.com.tw] Fix live extractor test

--- yt_dlp/extractor/ctsnews.py
+++ yt_dlp/extractor/ctsnews.py
@@ -1,13 +1,56 @@
 from .common import InfoExtractor
 from .youtube import YoutubeIE
-from ..utils import unified_timestamp
+from ..utils import (
+    ExtractorError,
+    traverse_obj,
+)
 
 
 class CtsNewsIE(InfoExtractor):
     IE_DESC = '華視新聞'
     _VALID_URL = r'https?://news\.cts\.com\.tw/[a-z]+/[a-z]+/\d+/(?P<id>\d+)\.html'
     _TESTS = [{
+        # Article video is hosted on YouTube
+        'url': 'https://news.cts.com.tw/cts/money/201501/201501291578003.html',
+        'md5': '3d78249510be24bccf77fba8686b9f24',
+        'info_dict': {
+            'id': 'OVbfO7d0_hQ',
+            'ext': 'mp4',
+            'title': 'iPhone6熱銷 蘋果財報亮眼',
+            'description': 'md5:f395d4f485487bb0f992ed2c4b07aa7d',
+            'thumbnail': r're:https?://i\.ytimg\.com/.+',
+            'timestamp': 1422514034,
+            'upload_date': '20150129',
+            'uploader': '華視新聞 CH52',
+            'uploader_id': '@CtsTw',
+            'uploader_url': 'https://www.youtube.com/@CtsTw',
+            'channel': '華視新聞 CH52',
+            'channel_id': 'UCDCJyLpbfgeVE9iZiEam-Kg',
+            'channel_url': 'https://www.youtube.com/channel/UCDCJyLpbfgeVE9iZiEam-Kg',
+            'channel_follower_count': int,
+            'channel_is_verified': True,
+            'duration': 50,
+            'view_count': int,
+            'age_limit': 0,
+            'availability': 'public',
+            'live_status': 'not_live',
+            'playable_in_embed': True,
+            'media_type': 'video',
+            'categories': ['News & Politics'],
+            'tags': ['華視', '新聞', 'iPhone6 IPHONE 蘋果 蘋概股'],
+        },
+        'params': {
+            'format': 'bestvideo[protocol=https][ext=mp4]/best[protocol=https]',
+        },
+        'add_ie': ['Youtube'],
+        'expected_warnings': [
+            'Remote component challenge solver script',
+            'No supported JavaScript runtime',
+            'n challenge solving failed',
+        ],
+    }, {
         'url': 'http://news.cts.com.tw/cts/international/201501/201501291578109.html',
+        'skip': 'Native CTSPlayer videos are no longer available',
         'md5': 'a9875cb790252b08431186d741beaabe',
         'info_dict': {
             'id': '201501291578109',
@@ -18,8 +61,8 @@ class CtsNewsIE(InfoExtractor):
             'upload_date': '20150129',
         },
     }, {
-        # News count not appear on page but still available in database
         'url': 'http://news.cts.com.tw/cts/international/201309/201309031304098.html',
+        'skip': 'Native CTSPlayer videos are no longer available',
         'md5': '3aee7e0df7cdff94e43581f54c22619e',
         'info_dict': {
             'id': '201309031304098',
@@ -30,55 +73,14 @@ class CtsNewsIE(InfoExtractor):
             'timestamp': 1378205880,
             'upload_date': '20130903',
         },
-    }, {
-        # With Youtube embedded video
-        'url': 'http://news.cts.com.tw/cts/money/201501/201501291578003.html',
-        'md5': 'e4726b2ccd70ba2c319865e28f0a91d1',
-        'info_dict': {
-            'id': 'OVbfO7d0_hQ',
-            'ext': 'mp4',
-            'title': 'iPhone6熱銷 蘋果財報亮眼',
-            'description': 'md5:f395d4f485487bb0f992ed2c4b07aa7d',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'upload_date': '20150128',
-            'uploader_id': 'TBSCTS',
-            'uploader': '中華電視公司',
-        },
-        'add_ie': ['Youtube'],
     }]
 
     def _real_extract(self, url):
         news_id = self._match_id(url)
-        page = self._download_webpage(url, news_id)
+        article = self._download_json(
+            f'https://www.cts.com.tw/api/news/{news_id}', news_id)
+        youtube_id = traverse_obj(article, ('data', 'article', 'youtubeId', {str}))
+        if not youtube_id:
+            raise ExtractorError('This news article does not contain a video', expected=True)
 
-        news_id = self._hidden_inputs(page).get('get_id')
-
-        if news_id:
-            mp4_feed = self._download_json(
-                'http://news.cts.com.tw/action/test_mp4feed.php',
-                news_id, note='Fetching feed', query={'news_id': news_id})
-            video_url = mp4_feed['source_url']
-        else:
-            self.to_screen('Not CTSPlayer video, trying Youtube...')
-            youtube_url = YoutubeIE._extract_url(page)
-
-            return self.url_result(youtube_url, ie='Youtube')
-
-        description = self._html_search_meta('description', page)
-        title = self._html_search_meta('title', page, fatal=True)
-        thumbnail = self._html_search_meta('image', page)
-
-        datetime_str = self._html_search_regex(
-            r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2})', page, 'date and time', fatal=False)
-        timestamp = None
-        if datetime_str:
-            timestamp = unified_timestamp(datetime_str) - 8 * 3600
-
-        return {
-            'id': news_id,
-            'url': video_url,
-            'title': title,
-            'description': description,
-            'thumbnail': thumbnail,
-            'timestamp': timestamp,
-        }
+        return self.url_result(youtube_id, YoutubeIE, youtube_id)
