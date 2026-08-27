6e64202248645a67d85cfa0bdcb8374616183df2
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 10:09:51 +0000

    [funk.net] Fix live extractor test

--- yt_dlp/extractor/funk.py
+++ yt_dlp/extractor/funk.py
@@ -5,6 +5,29 @@ from .nexx import NexxIE
 class FunkIE(InfoExtractor):
     _VALID_URL = r'https?://(?:(?:www|origin|play)\.)?funk\.net/(?:channel|playlist)/[^/?#]+/(?P<display_id>[0-9a-z-]+)-(?P<id>\d+)'
     _TESTS = [{
+        'url': 'https://play.funk.net/channel/mrwissen2go-8423/was-waere-wenn-hitler-den-krieg-gewonnen-haette-1509818',
+        'md5': '3bb00412af77a0014cfb980a814fd88d',
+        'info_dict': {
+            'id': '1509818',
+            'ext': 'mp4',
+            'title': 'Was wäre, wenn Hitler den Krieg gewonnen hätte?',
+            'alt_title': 'Was wäre, wenn Hitler den Krieg gewonnen hätte?',
+            'description': 'md5:f4ef9733bb98628f34684691b02db6b3',
+            'timestamp': 1525263173,
+            'upload_date': '20180502',
+            'duration': 842,
+            'cast': ['Objektiv Media GmbH'],
+            'thumbnail': 'https://assets.nexx.cloud/media/79/77/30/T3GNU5YT0GF2D5MxL.jpg',
+            'display_id': 'was-waere-wenn-hitler-den-krieg-gewonnen-haette',
+            'season_number': 0,
+            'season': 'Season 0',
+            'episode_number': 0,
+            'episode': 'Episode 0',
+        },
+        'params': {
+            'format': 'best[protocol=http]',
+        },
+    }, {
         'url': 'https://www.funk.net/channel/ba-793/die-lustigsten-instrumente-aus-dem-internet-teil-2-1155821',
         'skip': 'video gone',
         'md5': '8610449476156f338761a75391b0017d',
