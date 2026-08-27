c21217be34f9c91c3d7a5ce870fc02061228905e
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 17:13:02 +0000

    [kompas.com] Fix live extractor test

--- yt_dlp/extractor/jixie.py
+++ yt_dlp/extractor/jixie.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-from ..utils import clean_html, float_or_none, traverse_obj, try_call
+from ..utils import clean_html, float_or_none, traverse_obj, try_call, unified_timestamp
 
 
 class JixieBaseIE(InfoExtractor):
@@ -11,11 +11,13 @@ class JixieBaseIE(InfoExtractor):
 
     def _extract_data_from_jixie_id(self, display_id, video_id, webpage):
         json_data = self._download_json(
-            'https://apidam.jixie.io/api/public/stream', display_id,
-            query={'metadata': 'full', 'video_id': video_id})['data']
+            'https://apiv.kompas.com/jixie-stream', display_id,
+            query={'format': 'hls', 'metadata': 'full', 'video_id': video_id})['data']
 
         formats, subtitles = [], {}
-        for stream in json_data['streams']:
+        streams = json_data.get('streams') or traverse_obj(
+            json_data, ('platforms', ..., 'streams', ...)) or []
+        for stream in streams:
             if stream.get('type') == 'HLS':
                 fmt, sub = self._extract_m3u8_formats_and_subtitles(stream.get('url'), display_id, ext='mp4')
                 if json_data.get('drm'):
@@ -36,11 +38,14 @@ class JixieBaseIE(InfoExtractor):
             'display_id': display_id,
             'formats': formats,
             'subtitles': subtitles,
-            'title': json_data.get('title') or self._html_search_meta(['og:title', 'twitter:title'], webpage),
+            'title': (json_data.get('title')
+                      or traverse_obj(json_data, ('metadata', 'title'))
+                      or self._html_search_meta(['og:title', 'twitter:title'], webpage)),
             'description': (clean_html(traverse_obj(json_data, ('metadata', 'description')))
                             or self._html_search_meta(['description', 'og:description', 'twitter:description'], webpage)),
             'thumbnails': traverse_obj(json_data, ('metadata', 'thumbnails')),
             'duration': float_or_none(traverse_obj(json_data, ('metadata', 'duration'))),
+            'timestamp': unified_timestamp(traverse_obj(json_data, ('metadata', 'uploadedon'))),
             'tags': try_call(lambda: (json_data['metadata']['keywords'] or None).split(',')),
             'categories': try_call(lambda: (json_data['metadata']['categories'] or None).split(',')),
             'uploader_id': json_data.get('owner_id'),
--- yt_dlp/extractor/kompas.py
+++ yt_dlp/extractor/kompas.py
@@ -4,6 +4,21 @@ from .jixie import JixieBaseIE
 class KompasVideoIE(JixieBaseIE):
     _VALID_URL = r'https?://video\.kompas\.com/\w+/(?P<id>\d+)/(?P<slug>[\w-]+)'
     _TESTS = [{
+        'url': 'https://video.kompas.com/watch/1810471/tas-kw-mirip-banget-asli-beginicaramembedakannya',
+        'md5': '23bb19fefae5d93c138042c912d0cf7b',
+        'info_dict': {
+            'id': '1810471',
+            'ext': 'mp4',
+            'title': 'Tas KW Mirip Banget Asli, Begini\xa0Cara\xa0Membedakannya',
+            'description': 'md5:91b436419a9014bd98282c41f0adacc1',
+            'uploader_id': '9262bf2590d558736cac4fff7978fcb1',
+            'display_id': 'tas-kw-mirip-banget-asli-beginicaramembedakannya',
+            'duration': 368.0,
+            'timestamp': 1733570788,
+            'upload_date': '20241207',
+            'thumbnail': 'https://assets-studiohub.kompas.com/video2019/73f614858444241bddf143/e8fe67dab6537c1e9a75c73fc33c0b4a/p_e8fe67dab6537c1e9a75c73fc33c0b4a.jpg',
+        },
+    }, {
         'url': 'https://video.kompas.com/watch/164474/kim-jong-un-siap-kirim-nuklir-lawan-as-dan-korsel',
         'skip': 'video gone',
         'info_dict': {
