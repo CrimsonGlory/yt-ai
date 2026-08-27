fad11b9db5f4bb919ae6f34d7d6ebdd33a2122f0
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 12:36:23 +0000

    [helsinkikanava.fi] Fix live extractor test

--- yt_dlp/extractor/icareus.py
+++ yt_dlp/extractor/icareus.py
@@ -14,6 +14,7 @@ from ..utils import (
     url_or_none,
     urlencode_postdata,
 )
+from ..utils.traversal import require, traverse_obj
 
 
 class IcareusIE(InfoExtractor):
@@ -26,30 +27,34 @@ class IcareusIE(InfoExtractor):
         'suite.icareus.com',
         'videos.minifiddlers.org',
     )))
-    _VALID_URL = rf'(?P<base_url>https?://(?:www\.)?(?:{_DOMAINS}))/[^?#]+/player/[^?#]+\?(?:[^#]+&)?(?:assetId|eventId)=(?P<id>\d+)'
+    _VALID_URL = [
+        rf'(?P<base_url>https?://(?:www\.)?(?:{_DOMAINS}))/[^?#]+/player/[^?#]+\?(?:[^#]+&)?(?:assetId|eventId)=(?P<id>\d+)',
+        rf'(?P<base_url>https?://(?:www\.)?(?:{_DOMAINS}))/(?:[a-z]{{2}}(?:_[A-Z]{{2}})?/)?(?:video|event)/details/(?P<id>\d+)',
+    ]
     _TESTS = [{
         'url': 'https://www.helsinkikanava.fi/fi_FI/web/helsinkikanava/player/vod?assetId=68021894',
-        'md5': 'ca0b62ffc814a5411dfa6349cf5adb8a',
+        'md5': '3e048a91cd6be16d34b98a1548ceed27',
         'info_dict': {
             'id': '68021894',
             'ext': 'mp4',
             'title': 'Perheiden parhaaksi',
-            'description': 'md5:295785ea408e5ac00708766465cc1325',
-            'thumbnail': 'https://www.helsinkikanava.fi/image/image_gallery?img_id=68022501',
+            'description': 'md5:fe4e4ec742a34f53022f3a0409b0f6e7',
+            'thumbnail': 'https://dvcf59enpgt5y.cloudfront.net/image/image_gallery?img_id=68022501',
+            'duration': 293,
             'upload_date': '20200924',
-            'timestamp': 1600938300,
+            'timestamp': 1600938312,
         },
     }, {  # Recorded livestream
         'url': 'https://www.helsinkikanava.fi/fi/web/helsinkikanava/player/event/view?eventId=76241489',
-        'md5': '014327e69dfa7b949fcc861f6d162d6d',
+        'md5': 'a063a7ef36969ced44af9fe3d10a7f47',
         'info_dict': {
             'id': '76258304',
             'ext': 'mp4',
             'title': 'Helsingin kaupungin ja HUSin tiedotustilaisuus koronaepidemiatilanteesta 24.11.2020',
             'description': 'md5:3129d041c6fbbcdc7fe68d9a938fef1c',
-            'thumbnail': 'https://icareus-suite.secure2.footprint.net/image/image_gallery?img_id=76288630',
+            'thumbnail': 'https://dvcf59enpgt5y.cloudfront.net/image/image_gallery?img_id=76288630',
             'upload_date': '20201124',
-            'timestamp': 1606206600,
+            'timestamp': 1606206000,
         },
     }, {  # Non-m3u8 stream
         'url': 'https://suite.icareus.com/fi/web/westend-indians/player/vod?assetId=47567389',
@@ -64,6 +69,12 @@ class IcareusIE(InfoExtractor):
             'upload_date': '20200319',
             'timestamp': 1584658080,
         },
+    }, {
+        'url': 'https://www.helsinkikanava.fi/fi/video/details/68021894',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.helsinkikanava.fi/en/event/details/76241489',
+        'only_matching': True,
     }, {
         'url': 'https://asahitv.fi/fi/web/asahi/player/vod?assetId=89415818',
         'only_matching': True,
@@ -81,27 +92,16 @@ class IcareusIE(InfoExtractor):
         'only_matching': True,
     }]
 
-    def _real_extract(self, url):
-        base_url, temp_id = self._match_valid_url(url).groups()
-        webpage = self._download_webpage(url, temp_id)
-
-        video_id = self._search_regex(r"_icareus\['itemId'\]\s*=\s*'(\d+)'", webpage, 'video_id')
-        organization_id = self._search_regex(r"_icareus\['organizationId'\]\s*=\s*'(\d+)'", webpage, 'organization_id')
-
-        assets = self._download_json(
-            self._search_regex(r'var\s+publishingServiceURL\s*=\s*"(http[^"]+)";', webpage, 'api_base'),
-            video_id, data=urlencode_postdata({
-                'version': '03',
-                'action': 'getAssetPlaybackUrls',
-                'organizationId': organization_id,
-                'assetId': video_id,
-                'token': self._search_regex(r"_icareus\['token'\]\s*=\s*'([a-f0-9]+)'", webpage, 'icareus_token'),
-            }))
-
-        subtitles = {
-            remove_end(sdesc.split(' ')[0], ':'): [{'url': url_or_none(surl)}]
-            for _, sdesc, surl in assets.get('subtitles') or []
-        }
+    def _extract_playback(self, assets, video_id):
+        subtitles = {}
+        for item in assets.get('subtitles') or []:
+            if len(item) < 3:
+                continue
+            surl = url_or_none(item[2])
+            if not surl:
+                continue
+            lang = remove_end((item[1] or '').split(' ')[0], ':') or 'und'
+            subtitles.setdefault(lang, []).append({'url': surl})
 
         formats = [{
             'format': item.get('name'),
@@ -132,39 +132,89 @@ class IcareusIE(InfoExtractor):
                     **parse_resolution(fmt),
                 })
 
-        info, token, live_title = self._search_json_ld(webpage, video_id, default={}), None, None
-        if not info:
-            token = self._search_regex(
-                r'data\s*:\s*{action:"getAsset".*?token:\'([a-f0-9]+)\'}', webpage, 'token', default=None)
-            if not token:
-                live_title = get_element_by_class('unpublished-info-item future-event-title', webpage)
+        return formats, subtitles
 
-        if token:
-            metadata = self._download_json(
-                f'{base_url}/icareus-suite-api-portlet/publishing',
-                video_id, fatal=False, data=urlencode_postdata({
+    def _extract_nextjs(self, webpage, temp_id):
+        nextjs = self._search_nextjs_v13_data(webpage, temp_id)
+        player = traverse_obj(nextjs, (
+            lambda _, v: isinstance(v, dict) and isinstance(v.get('videoInfo'), dict),
+            any, {require('player data')}))
+        asset = player.get('asset') or {}
+        event = traverse_obj(nextjs, (
+            lambda _, v: isinstance(v, dict) and v.get('eventId') and v.get('startTime'),
+            any)) or {}
+
+        return (
+            str_or_none(player.get('assetId') or player.get('id')) or temp_id,
+            player['videoInfo'],
+            {
+                'title': asset.get('name') or self._og_search_title(webpage, default=None),
+                'description': asset.get('description') or self._og_search_description(webpage),
+                'timestamp': int_or_none(
+                    asset.get('created') or asset.get('date') or event.get('startTime'), scale=1000),
+                'duration': int_or_none(asset.get('duration')),
+                'thumbnail': url_or_none(
+                    asset.get('thumbnailMedium') or player['videoInfo'].get('thumbnail')),
+            },
+        )
+
+    def _real_extract(self, url):
+        base_url, temp_id = self._match_valid_url(url).group('base_url', 'id')
+        webpage = self._download_webpage(url, temp_id)
+
+        video_id = self._search_regex(
+            r"_icareus\['itemId'\]\s*=\s*'(\d+)'", webpage, 'video_id', default=None)
+        if video_id:
+            organization_id = self._search_regex(
+                r"_icareus\['organizationId'\]\s*=\s*'(\d+)'", webpage, 'organization_id')
+            assets = self._download_json(
+                self._search_regex(r'var\s+publishingServiceURL\s*=\s*"(http[^"]+)";', webpage, 'api_base'),
+                video_id, data=urlencode_postdata({
                     'version': '03',
-                    'action': 'getAsset',
+                    'action': 'getAssetPlaybackUrls',
                     'organizationId': organization_id,
                     'assetId': video_id,
-                    'languageId': 'en_US',
-                    'userId': '0',
-                    'token': token,
-                })) or {}
-            info = {
-                'title': metadata.get('name'),
-                'description': metadata.get('description'),
-                'timestamp': int_or_none(metadata.get('date'), scale=1000),
-                'duration': int_or_none(metadata.get('duration')),
-                'thumbnail': url_or_none(metadata.get('thumbnailMedium')),
-            }
-        elif live_title:  # Recorded livestream
-            info = {
-                'title': live_title,
-                'description': get_element_by_class('unpublished-info-item future-event-description', webpage),
-                'timestamp': int_or_none(self._search_regex(
-                    r'var startEvent\s*=\s*(\d+);', webpage, 'uploadDate', fatal=False), scale=1000),
-            }
+                    'token': self._search_regex(
+                        r"_icareus\['token'\]\s*=\s*'([a-f0-9]+)'", webpage, 'icareus_token'),
+                }))
+
+            info, token, live_title = self._search_json_ld(webpage, video_id, default={}), None, None
+            if not info:
+                token = self._search_regex(
+                    r'data\s*:\s*{action:"getAsset".*?token:\'([a-f0-9]+)\'}', webpage, 'token', default=None)
+                if not token:
+                    live_title = get_element_by_class('unpublished-info-item future-event-title', webpage)
+
+            if token:
+                metadata = self._download_json(
+                    f'{base_url}/icareus-suite-api-portlet/publishing',
+                    video_id, fatal=False, data=urlencode_postdata({
+                        'version': '03',
+                        'action': 'getAsset',
+                        'organizationId': organization_id,
+                        'assetId': video_id,
+                        'languageId': 'en_US',
+                        'userId': '0',
+                        'token': token,
+                    })) or {}
+                info = {
+                    'title': metadata.get('name'),
+                    'description': metadata.get('description'),
+                    'timestamp': int_or_none(metadata.get('date'), scale=1000),
+                    'duration': int_or_none(metadata.get('duration')),
+                    'thumbnail': url_or_none(metadata.get('thumbnailMedium')),
+                }
+            elif live_title:  # Recorded livestream
+                info = {
+                    'title': live_title,
+                    'description': get_element_by_class('unpublished-info-item future-event-description', webpage),
+                    'timestamp': int_or_none(self._search_regex(
+                        r'var startEvent\s*=\s*(\d+);', webpage, 'uploadDate', fatal=False), scale=1000),
+                }
+        else:
+            video_id, assets, info = self._extract_nextjs(webpage, temp_id)
+
+        formats, subtitles = self._extract_playback(assets, video_id)
 
         thumbnails = info.get('thumbnails') or [{
             'url': url_or_none(info.get('thumbnail') or assets.get('thumbnail')),
