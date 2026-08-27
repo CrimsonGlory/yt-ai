39bdb20c03d99db1ab64cabf8fd09d300fc14bdb
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 23:25:02 +0000

    [muse.ai] Fix live extractor test

--- yt_dlp/extractor/museai.py
+++ yt_dlp/extractor/museai.py
@@ -13,10 +13,27 @@ from ..utils import (
 
 
 class MuseAIIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?muse\.ai/(?:v|embed)/(?P<id>\w+)'
+    IE_DESC = 'muse.ai (now Skiv)'
+    _VALID_URL = r'https?://(?:www\.)?(?:muse\.ai|skiv\.com)/(?:v|embed)/(?P<id>\w+)'
     _TESTS = [{
+        'url': 'https://skiv.com/v/gQ4gGAA-0756',
+        'md5': '52dbfc78e865e56dc19a1715badc35e8',
+        'info_dict': {
+            'id': 'gQ4gGAA',
+            'ext': 'mp4',
+            'title': '0756',
+            'description': 'md5:0ca1483f9aac423e9a96ad00bb3a0785',
+            'uploader': 'Aerial.ie',
+            'uploader_id': 'aerial',
+            'upload_date': '20210306',
+            'timestamp': 1615072842,
+            'duration': 21.4,
+            'view_count': int,
+            'availability': 'public',
+        },
+    }, {
         'url': 'https://muse.ai/embed/YdTWvUW',
-        'skip': 'Site no longer exists or is broken',
+        'skip': 'Video gone',
         'md5': 'f994f9a38be1c3aaf9e37cbd7d76fe7c',
         'info_dict': {
             'id': 'YdTWvUW',
@@ -33,45 +50,33 @@ class MuseAIIE(InfoExtractor):
         },
     }, {
         'url': 'https://muse.ai/v/gQ4gGAA-0756',
-        'skip': 'Site no longer exists or is broken',
-        'md5': '52dbfc78e865e56dc19a1715badc35e8',
-        'info_dict': {
-            'id': 'gQ4gGAA',
-            'ext': 'mp4',
-            'title': '0756',
-            'description': 'md5:0ca1483f9aac423e9a96ad00bb3a0785',
-            'uploader': 'Aerial.ie',
-            'uploader_id': 'aerial',
-            'upload_date': '20210306',
-            'timestamp': 1615072842,
-            'duration': 21.4,
-            'view_count': int,
-            'availability': 'public',
-        },
+        'only_matching': True,
+    }, {
+        'url': 'https://skiv.com/embed/sLFaX4p',
+        'only_matching': True,
     }]
     _WEBPAGE_TESTS = [{
-        'url': 'https://muse.ai/docs',
-        'playlist_mincount': 4,
+        'url': 'https://skiv.com/docs',
+        'playlist_mincount': 3,
         'info_dict': {
             'id': 'docs',
-            'title': 'muse.ai | docs',
-            'description': 'md5:6c0293431481582739c82ee8902687fa',
-            'age_limit': 0,
-            'thumbnail': 'https://muse.ai/static/imgs/poster-img-docs.png',
+            'title': 'Skiv | docs',
+            'description': 'md5:2bacf5c81258fe02070f11fdc8103bc9',
+            'thumbnail': 'https://skiv.com/static/imgs/poster-img-docs.png',
         },
         'params': {'allowed_extractors': ['all', '-html5']},
     }]
-    _EMBED_REGEX = [r'<iframe[^>]*\bsrc=["\'](?P<url>https://muse\.ai/embed/\w+)']
+    _EMBED_REGEX = [r'<iframe[^>]*\bsrc=["\'](?P<url>https://(?:muse\.ai|skiv\.com)/embed/\w+)']
 
     @classmethod
     def _extract_embed_urls(cls, url, webpage):
         yield from super()._extract_embed_urls(url, webpage)
-        for embed_id in re.findall(r'<script>[^<]*\bMusePlayer\(\{[^}<]*\bvideo:\s*["\'](\w+)["\']', webpage):
-            yield f'https://muse.ai/embed/{embed_id}'
+        for embed_id in re.findall(r'<script>[^<]*\b(?:Muse|Skiv)Player\(\{[^}<]*\bvideo:\s*["\'](\w+)["\']', webpage):
+            yield f'https://skiv.com/embed/{embed_id}'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        webpage = self._download_webpage(f'https://muse.ai/embed/{video_id}', video_id)
+        webpage = self._download_webpage(f'https://skiv.com/embed/{video_id}', video_id)
         data = self._search_json(
             r'player\.setData\(', webpage, 'player data', video_id, transform_source=js_to_json)
 
