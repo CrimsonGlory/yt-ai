f0882e949af99dbb5703d69ccf213b0ea7662cbc
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 18:27:34 +0000

    [learningonscreen.ac.uk] Fix live extractor test

--- yt_dlp/extractor/learningonscreen.py
+++ yt_dlp/extractor/learningonscreen.py
@@ -17,7 +17,7 @@ class LearningOnScreenIE(InfoExtractor):
     _VALID_URL = r'https?://learningonscreen\.ac\.uk/ondemand/index\.php/prog/(?P<id>\w+)'
     _TESTS = [{
         'url': 'https://learningonscreen.ac.uk/ondemand/index.php/prog/005D81B2?bcast=22757013',
-        'skip': 'Login required',
+        'skip': 'Requires UK institutional ERA membership login',
         'info_dict': {
             'id': '005D81B2',
             'ext': 'mp4',
