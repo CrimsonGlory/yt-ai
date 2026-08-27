474821067ecbe18d4bf65ffe7f2163b578db3f7c
CrimsonGlory <javierbassi@gmail.com>
2026-08-27 08:50:01 +0000

    [filmweb.no] Fix live extractor test

--- yt_dlp/extractor/filmweb.py
+++ yt_dlp/extractor/filmweb.py
@@ -1,11 +1,35 @@
+import json
+
 from .common import InfoExtractor
+from .youtube import YoutubeIE
+from ..utils import (
+    ExtractorError,
+    traverse_obj,
+)
 
 
 class FilmwebIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?filmweb\.no/(?P<type>trailere|filmnytt)/article(?P<id>\d+)\.ece'
-    _TEST = {
+    _VALID_URL = r'https?://(?:www\.)?filmweb\.no/(?:(?:trailere|filmnytt)/article|(?:film|filmnytt)/)(?P<id>[^/?#.]+)(?:\.ece)?'
+    _TESTS = [{
+        'url': 'https://www.filmweb.no/filmnytt/forste-trailer-markens-grode',
+        'md5': '16bd233dd346c486cff93598a349f0d8',
+        'info_dict': {
+            'id': '130565033',
+            'ext': 'mp4',
+            'title': 'Markens grøde',
+            'timestamp': 1786964554,
+            'upload_date': '20260817',
+            'uploader_id': '12639962',
+            'uploader': 'Ted Sanne',
+            'duration': 126,
+            'thumbnail': r're:https?://.*',
+            'view_count': int,
+            'comment_count': int,
+        },
+        'add_ie': ['TwentyThreeVideo'],
+    }, {
         'url': 'http://www.filmweb.no/trailere/article1264921.ece',
-        'skip': 'video gone',
+        'skip': 'Old article URLs are gone after the site redesign',
         'md5': 'e353f47df98e557d67edaceda9dece89',
         'info_dict': {
             'id': '13033574',
@@ -16,24 +40,72 @@ class FilmwebIE(InfoExtractor):
             'uploader_id': '12639966',
             'uploader': 'Live Roaldset',
         },
-    }
+    }, {
+        'url': 'https://www.filmweb.no/film/UIP20241680',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.filmweb.no/filmnytt/dune-part-three-forste-trailer',
+        'only_matching': True,
+    }]
+    _CLIP_QUERY = '''query ($clipId: Int) {
+        movieQuery {
+            getMovieClip(clipId: $clipId, autoplay: false) {
+                clipTitle
+                id
+                embedCode
+            }
+        }
+    }'''
 
-    def _real_extract(self, url):
-        article_type, article_id = self._match_valid_url(url).groups()
-        if article_type == 'filmnytt':
-            webpage = self._download_webpage(url, article_id)
-            article_id = self._search_regex(r'data-videoid="(\d+)"', webpage, 'article id')
-        embed_code = self._download_json(
-            'https://www.filmweb.no/template_v2/ajax/json_trailerEmbed.jsp',
-            article_id, query={
-                'articleId': article_id,
-            })['embedCode']
+    def _extract_twentythree(self, clip_id, display_id):
+        clip = traverse_obj(self._download_json(
+            'https://movieinfoqs.filmweb.no/graphql', display_id,
+            data=json.dumps({
+                'query': self._CLIP_QUERY,
+                'variables': {'clipId': int(clip_id)},
+            }).encode(),
+            headers={
+                'Content-Type': 'application/json',
+                'Origin': 'https://www.filmweb.no',
+            }), ('data', 'movieQuery', 'getMovieClip', {dict}))
+        embed_code = traverse_obj(clip, ('embedCode', {str}))
+        if not embed_code:
+            raise ExtractorError('Unable to extract trailer embed', expected=True)
+
         iframe_url = self._proto_relative_url(self._search_regex(
             r'<iframe[^>]+src="([^"]+)', embed_code, 'iframe url'))
+        return self.url_result(
+            iframe_url, 'TwentyThreeVideo', str(clip.get('id') or clip_id),
+            clip.get('clipTitle'), url_transparent=True)
 
-        return {
-            '_type': 'url_transparent',
-            'id': article_id,
-            'url': iframe_url,
-            'ie_key': 'TwentyThreeVideo',
-        }
+    def _real_extract(self, url):
+        display_id = self._match_id(url)
+        if '/article' in url:
+            if '/filmnytt/' in url:
+                webpage = self._download_webpage(url, display_id)
+                display_id = self._search_regex(r'data-videoid="(\d+)"', webpage, 'article id')
+            embed_code = self._download_json(
+                'https://www.filmweb.no/template_v2/ajax/json_trailerEmbed.jsp',
+                display_id, query={
+                    'articleId': display_id,
+                })['embedCode']
+            iframe_url = self._proto_relative_url(self._search_regex(
+                r'<iframe[^>]+src="([^"]+)', embed_code, 'iframe url'))
+            return self.url_result(
+                iframe_url, 'TwentyThreeVideo', display_id, url_transparent=True)
+
+        webpage = self._download_webpage(url, display_id)
+        clip_id = self._search_regex(
+            (r'\\"videoId\\":\\"(\d+)\\"',
+             r'"videoId"\s*:\s*"(\d+)"',
+             r'twentythreeEmbed[^}]+videoId\\?":(\d+)'),
+            webpage, 'clip id', default=None)
+        if clip_id:
+            return self._extract_twentythree(clip_id, display_id)
+
+        youtube_id = self._search_regex(
+            r'youtubeId\\?":\\?"([0-9A-Za-z_-]{11})', webpage, 'youtube id', default=None)
+        if youtube_id:
+            return self.url_result(youtube_id, YoutubeIE, youtube_id)
+
+        raise ExtractorError('No video found on this page', expected=True)
