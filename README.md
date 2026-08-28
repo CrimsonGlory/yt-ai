c735911d8e077ea33eec5b0b5569daab0409b794
CrimsonGlory <javierbassi@gmail.com>
2026-08-28 17:07:12 +0000

    [scrolller.com] Fix live extractor test

--- README.md
+++ README.md
@@ -2460,6 +2460,7 @@ yt-ai is a fork of [yt-dlp](https://github.com/yt-dlp/yt-dlp). Download defaults
     * **sbs**: Fall back to the public FOS `mpx/video/stream` HLS API when `video_smil` is gone
     * **sbs.co.kr**: Use the current `apis.sbs.co.kr/play-api` host instead of the dead `api.play.sbs.co.kr` endpoint
     * **screencastomatic**: Extract ScreenPal `player/stream` MP4s after the Screencast-O-Matic rebrand
+    * **scrolller**: Use the current `api.scrolller.com/admin` GraphQL `getPost` query
     * **showroom**: Use the public room API instead of Nuxt + login cookie
     * **tfo**: Fall back to Next.js v13 data
     * **theguardian:podcast**: Extract audio URL and author from JSON-LD
--- yt_dlp/extractor/scrolller.py
+++ yt_dlp/extractor/scrolller.py
@@ -1,99 +1,111 @@
 import json
 
 from .common import InfoExtractor
-from ..utils import determine_ext, int_or_none
+from ..utils import (
+    ExtractorError,
+    determine_ext,
+    int_or_none,
+    url_or_none,
+)
+from ..utils.traversal import traverse_obj
 
 
 class ScrolllerIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?scrolller\.com/(?P<id>[\w-]+)'
     _TESTS = [{
         'url': 'https://scrolller.com/a-helping-hand-1k9pxikxkw',
-        'skip': 'video gone',
+        'md5': 'cee62f8f16685fdd3558248e13a6c1af',
         'info_dict': {
             'id': 'a-helping-hand-1k9pxikxkw',
             'ext': 'mp4',
-            'thumbnail': 'https://zepto.scrolller.com/a-helping-hand-3ty9q8x094-540x960.jpg',
+            'thumbnail': r're:https://images\.scrolller\.com/.+',
             'title': 'A helping hand',
             'age_limit': 0,
         },
     }, {
         'url': 'https://scrolller.com/tigers-chasing-a-drone-c5d1f2so6j',
-        'skip': 'video gone',
         'info_dict': {
             'id': 'tigers-chasing-a-drone-c5d1f2so6j',
             'ext': 'mp4',
-            'thumbnail': 'https://zepto.scrolller.com/tigers-chasing-a-drone-az9pkpguwe-540x303.jpg',
+            'thumbnail': r're:https://images\.scrolller\.com/.+',
             'title': 'Tigers chasing a drone',
             'age_limit': 0,
         },
+        'params': {'skip_download': True},
     }, {
         'url': 'https://scrolller.com/baby-rhino-smells-something-9chhugsv9p',
-        'skip': 'video gone',
         'info_dict': {
             'id': 'baby-rhino-smells-something-9chhugsv9p',
             'ext': 'mp4',
-            'thumbnail': 'https://atto.scrolller.com/hmm-whats-that-smell-bh54mf2c52-300x224.jpg',
+            'thumbnail': r're:https://images\.scrolller\.com/.+',
             'title': 'Baby rhino smells something',
             'age_limit': 0,
         },
+        'params': {'skip_download': True},
     }, {
         'url': 'https://scrolller.com/its-all-fun-and-games-cco8jjmoh7',
-        'skip': 'video gone',
         'info_dict': {
             'id': 'its-all-fun-and-games-cco8jjmoh7',
             'ext': 'mp4',
-            'thumbnail': 'https://atto.scrolller.com/its-all-fun-and-games-3amk9vg7m3-540x649.jpg',
+            'thumbnail': r're:https://images\.scrolller\.com/.+',
             'title': 'It\'s all fun and games...',
             'age_limit': 0,
         },
+        'params': {'skip_download': True},
     }, {
         'url': 'https://scrolller.com/may-the-force-be-with-you-octokuro-yeytg1fs7a',
-        'skip': 'video gone',
         'info_dict': {
             'id': 'may-the-force-be-with-you-octokuro-yeytg1fs7a',
             'ext': 'mp4',
-            'thumbnail': 'https://thumbs2.redgifs.com/DarkStarchyNautilus-poster.jpg',
+            'thumbnail': r're:https://.+\.(?:jpg|jpeg|webp)',
             'title': 'May the force be with you (Octokuro)',
             'age_limit': 18,
         },
+        'params': {'skip_download': True},
     }]
+    _GRAPHQL_QUERY = '''
+        query SubredditPostQuery($url: String!) {
+            getPost(data: { url: $url }) {
+                title
+                isNsfw
+                mediaSources {
+                    url
+                    width
+                    height
+                }
+            }
+        }'''
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        query = {
-            'query': '''{
-                getSubredditPost(url:"/%s"){
-                    id
-                    title
-                    isNsfw
-                    mediaSources{
-                        url
-                        width
-                        height
-                    }
-                }
-            }''' % video_id,  # noqa: UP031
-        }
-
-        video_data = self._download_json(
-            'https://api.scrolller.com/api/v2/graphql', video_id, data=json.dumps(query).encode(),
-            headers={'Content-Type': 'application/json'})['data']['getSubredditPost']
+        video_data = traverse_obj(self._download_json(
+            'https://api.scrolller.com/admin', video_id,
+            data=json.dumps({
+                'query': self._GRAPHQL_QUERY,
+                'variables': {'url': f'/{video_id}'},
+            }).encode(),
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+            }), ('data', 'getPost', {dict}))
+        if not video_data:
+            raise ExtractorError('Unable to extract post', expected=True, video_id=video_id)
 
         formats, thumbnails = [], []
-        for source in video_data['mediaSources']:
-            if determine_ext(source.get('url')) in ('jpg', 'png'):
-                thumbnails.append({
-                    'url': source['url'],
-                    'width': int_or_none(source.get('width')),
-                    'height': int_or_none(source.get('height')),
-                })
-            elif source.get('url'):
-                formats.append({
-                    'url': source['url'],
-                    'width': int_or_none(source.get('width')),
-                    'height': int_or_none(source.get('height')),
-                })
+        for source in traverse_obj(video_data, ('mediaSources', ..., {dict})):
+            media_url = url_or_none(source.get('url'))
+            if not media_url:
+                continue
+            media = {
+                'url': media_url,
+                'width': int_or_none(source.get('width')),
+                'height': int_or_none(source.get('height')),
+            }
+            if determine_ext(media_url) in ('jpg', 'jpeg', 'png', 'webp'):
+                thumbnails.append(media)
+            else:
+                formats.append(media)
 
         if not formats:
             self.raise_no_formats('There is no video.', expected=True, video_id=video_id)
